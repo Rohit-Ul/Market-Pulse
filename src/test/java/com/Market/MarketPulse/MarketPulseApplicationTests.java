88fca63654e0bf6c671d1031f2deabb01f5d65dc
rohit74899 <rohitlande02@gmail.com>
2026-03-03 18:29:07 +0530

    chore(file) : update .gitignore

---
+++ src/test/java/com/Market/MarketPulse/MarketPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Market.MarketPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
